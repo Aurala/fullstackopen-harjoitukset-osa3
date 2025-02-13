1eee3d8e88116328aea6d49050d2ff516b196ec9
Markus Aurala <markus.aurala@gmail.com>
2025-02-13 18:10:48 +0200

    puhelinluettelon backend, step1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+const express = require('express')
+const app = express()
+
+// Read the database from a file ./db.json (to be replaced with a real database later on)
+const data = require('./db.json')
+console.log("Database read from file:", data);
+
+// Return the full data in JSON
+app.get('/api/persons', (request, response) => {
+    response.json(data)
+})
+
+// Start the server
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
