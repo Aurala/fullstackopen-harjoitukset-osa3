4702aaa6c809a0e4fef040a15ac03d1e121b8d75
Markus Aurala <markus.aurala@gmail.com>
2025-02-13 18:26:15 +0200

    puhelinluettelon backend, step2

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 
 // Read the database from a file ./db.json (to be replaced with a real database later on)
-const data = require('./db.json')
-console.log("Database read from file:", data);
+const data = JSON.parse(fs.readFileSync('./db.json'))
+
+// Return an info page
+app.get('/info', (request, response) => {
+  const info =
+    `Phonebook has info for ${data.persons.length} people<br>
+    ${new Date()}`
+
+  response.send(info)
+})
 
 // Return the full data in JSON
 app.get('/api/persons', (request, response) => {
-    response.json(data)
+  response.json(data.persons)
 })
 
 // Start the server
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 })
