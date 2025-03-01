b973d5003ea4b49cf6c12e8448797bd8eafd8aa5
Markus Aurala <markus.aurala@gmail.com>
2025-03-01 17:10:13 +0200

    puhelinluettelon backend, step7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const fs = require('fs')
 const app = express()
 
+app.use(morgan('tiny'))
 app.use(express.json())
 
 // Read the database from a file ./db.json (to be replaced with a real database later on)
