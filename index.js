21de7dd8c88e422bfb26edc1946137dac89bb177
Markus Aurala <markus.aurala@gmail.com>
2025-03-29 17:29:46 +0200

    puhelinluettelo ja tietokanta, step1 & step2

--- index.js
+++ index.js
@@ -1,77 +1,91 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const fs = require('fs')
+const Person = require('./models/person')
 const app = express()
 
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError) {
+    console.error('Invalid JSON:', err.stack)
+    return res.status(400).send({ error: 'Invalid JSON' })
+  }
+  next(err)
+})
+
 morgan.token('body', req => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// Read the database from a file ./db.json (to be replaced with a real database later on)
-// Note: Currently read-only, no changes are saved to the file
-const data = JSON.parse(fs.readFileSync('./db.json'))
-
 // Return an info page
 app.get('/info', (request, response) => {
-  const info =
-    `Phonebook has info for ${data.persons.length} people<br>
-    ${new Date()}`
-
-  response.send(info)
+  Person.find({}).then(result => {
+    const info =
+      `Phonebook has info for ${result.length} people<br>
+      ${new Date()}`
+    response.send(info)
+  })
 })
 
-// Return the full data in JSON
+// Return all persons
 app.get('/api/persons', (request, response) => {
-  response.json(data.persons)
+  Person.find({}).then(result => {
+    console.log('Persons:', result)
+    response.json(result)
+  })
 })
 
-// Return a single person in JSON, status code 404 if not found
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = data.persons.find(person => person.id === id)
-
-  if (!person) {
-    return response.status(404).end()
-  } else {
-    response.json(person)
-  }
+// Return a single person, status code 404 if not found
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
-// Add a new person (with a random id), returns status code 201 if successful, 400 if not (missing data or a duplicate)
-// Note: Does not add the person to the database file (yet), only to the memory
-app.post('/api/persons', (request, response) => {
+// Add a new person, returns status code 201 if successful, 400 if not (missing data)
+app.post('/api/persons', (request, response, next) => {
   const person = request.body
 
-  if (!person || !person.name || !person.number) {
-    return response.status(400).json({ error: 'name or number missing' })
-  }
+  const newPerson = new Person({
+    name: person.name,
+    number: person.number,
+  })
 
-  if (data.persons.find(p => p.name === person.name)) {
-    return response.status(400).json({ error: 'user exists already' })
-  }
-
-  person.id = Math.floor(Math.random() * 1000000).toString()
-
-  data.persons = data.persons.concat(person)
-  response.status(201).json(person)
+  newPerson.save().then(savedPerson => {
+    console.log('Person saved:', savedPerson.id, savedPerson.name, savedPerson.number)
+    response.status(201).json(savedPerson)
+  }).catch(error => next(error))
 })
 
 // Delete a single person, status code 204 if successful, 404 if not found
-// Note: Does not delete the person from the database file (yet), only from the memory
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-  const person = data.persons.find(person => person.id === id)
 
-  if (!person) {
-    return response.status(404).end()
-  } else {
-    data.persons = data.persons.filter(person => person.id !== id)
-    response.status(204).end()
+  Person.findByIdAndDelete(id).then(result => {
+    if (result) {
+      console.log('Deleted person:', result.name)
+      response.status(204).end()
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.name, err.message)
+  if (err.name === 'CastError') {
+    return res.status(400).json({ error: 'id not a valid MongoDB ObjectId' })
   }
+  next(err)
 })
 
 // Start the server
